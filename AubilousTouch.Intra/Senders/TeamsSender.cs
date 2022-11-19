c8e3bb71cfda676c65ffd05abc2e04142a927bb1
Bruno Aguiar <braguiar1985@gmail.com>
2022-11-19 16:00:42 +0000

    TelegramSender

---
+++ AubilousTouch.Intra/Senders/TeamsSender.cs
@@ -0,0 +1,15 @@
+﻿using AubilousTouch.Core.Interfaces;
+using AubilousTouch.Core.Models;
+using System;
+using System.Threading.Tasks;
+
+namespace AubilousTouch.Intra.Senders
+{
+    public class TeamsSender : IMessageSender
+    {
+        public Task SendMessage(ChannelEmployeeMessage channelEmployeeMesssage)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ AubilousTouch.Intra/Senders/TelegramSender.cs
@@ -0,0 +1,20 @@
+﻿using AubilousTouch.Core.Interfaces;
+using AubilousTouch.Core.Models;
+using System;
+using System.Threading.Tasks;
+using Telegram.Bot;
+
+namespace AubilousTouch.Intra.Senders
+{
+    public class TelegramSender : IMessageSender
+    {
+        public async Task SendMessage(ChannelEmployeeMessage channelEmployeeMesssage)
+        {
+            var apiKey = "22855303";
+            
+            var bot = new TelegramBotClient(apiKey);
+
+            var result = bot.SendTextMessageAsync(869805046, "Aubilous - Está na hora de procurar um emprego! Você está demitido :D");
+        }
+    }
+}
