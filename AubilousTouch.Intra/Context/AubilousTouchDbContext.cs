e083f60af97c5e1aeefa2ac059cbc986664e7511
Tácio Dias Palhão Mendes <taciomendes@hotmail.com>
2022-11-19 11:23:15 +0000

    EF Base

---
+++ AubilousTouch.Intra/Context/AubilousTouchDbContext.cs
@@ -0,0 +1,29 @@
+﻿using AubilousTouch.Core.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AubilousTouch.Intra.Context
+{
+    public class AubilousTouchDbContext : DbContext
+    {
+        public AubilousTouchDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<MessagesChannel> MessagesChannels { get; set; }
+        public DbSet<MessagesChannelPerEmployee> MessagesChannelPerEmployees { get; set; }
+        public DbSet<MessagesGroup> MessagesGroup { get; set; }
+        public DbSet<MessagesGroupPerEmployee> MessagesGroupPerEmployee { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //TODO: Conforme necessário
+        }
+    }
+}
