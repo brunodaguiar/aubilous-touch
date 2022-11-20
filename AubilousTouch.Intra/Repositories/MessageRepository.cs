ff4c908fff6cccd6a5bf4b953af43fdddfc8f841
Tácio Dias Palhão Mendes <taciomendes@hotmail.com>
2022-11-20 10:31:55 +0000

    movendo repositories pro lugar certo

--- AubilousTouch.Intra/Repositories/MessageRepository.cs
+++ AubilousTouch.Intra/Repositories/MessageRepository.cs
@@ -1,8 +1,9 @@
 ﻿using AubilousTouch.Core.Interfaces.Repositories;
 using AubilousTouch.Core.Models;
 using AubilousTouch.Intra.Context;
+using AubilousTouch.Intra.Repositories.Base;
 
-namespace AubilousTouch.Intra.Repositories.Base
+namespace AubilousTouch.Intra.Repositories
 {
     public class MessageRepository : RepositoryBase<Message>, IMessageRepository
     {
--- AubilousTouch.Intra/Repositories/MessagesChannelPerEmployeeRepository.cs
+++ AubilousTouch.Intra/Repositories/MessagesChannelPerEmployeeRepository.cs
@@ -1,8 +1,9 @@
 ﻿using AubilousTouch.Core.Interfaces.Repositories;
 using AubilousTouch.Core.Models;
 using AubilousTouch.Intra.Context;
+using AubilousTouch.Intra.Repositories.Base;
 
-namespace AubilousTouch.Intra.Repositories.Base
+namespace AubilousTouch.Intra.Repositories
 {
     public class MessagesChannelPerEmployeeRepository : RepositoryBase<MessagesChannelPerEmployee>, IMessagesChannelPerEmployeeRepository
     {
